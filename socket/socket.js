caeb41c522136cf1975fc8b36daf7f55ccba9015
mushahid hussain <khokharmushahidhussain@gmail.com>
2024-02-05 01:52:08 +0500

    pp

--- socket/socket.js
+++ socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://mern-back-7d3f.onrender.com",
         methods: ["GET", "POST"],
     },
 });
